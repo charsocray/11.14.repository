d0e58d533cd35311d5a9fc39894cf0f4ab66ad0e
Hailstone <duggc316@newschool.edu>
2023-11-14 17:11:10 -0500

    text edits

---
+++ script.js
@@ -0,0 +1,13 @@
+$("document").ready(function() {
+
+    $("h3").click(function() {
+      //  alert("h1 was clicked!!")
+    //    console.log("h1 was clicked AGAIN")
+        $("p").toggleClass("fancy");
+    });
+
+
+
+
+
+});
